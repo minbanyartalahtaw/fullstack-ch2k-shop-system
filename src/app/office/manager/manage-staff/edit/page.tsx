b813280d9c3a2bb1c49afc9c5bac1f335448e7c5
Banyar <minbanyartalahtaw@proton.me>
2025-09-14 07:07:34 +0700

    edit staff page

--- src/app/office/manager/manage-staff/edit/page.tsx
+++ src/app/office/manager/manage-staff/edit/page.tsx
@@ -1,7 +1,14 @@
-export default function Page() {
+
+import prisma from '@/lib/prisma'
+import StaffEditForm from "./components/StaffEditForm"
+import { getStaff, Staff } from "./action"
+
+export default async function StaffEditPage() {
+    const StaffData: Staff[] = await getStaff()
+    if (StaffData.length === 0) return null
     return (
-        <div>
-            <h1>Edit Staff</h1>
+        <div className="px-1">
+            <StaffEditForm staffData={StaffData} />
         </div>
     )
 }
